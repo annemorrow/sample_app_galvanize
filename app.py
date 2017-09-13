a802beef695079144410784685d478d421412bf1
Anne Morrow <anne.moore2008@gmail.com>
2017-09-13 16:03:10 -0600

    change port

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def index():
 # no more routing blocks
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8105, debug=True)
     # the combination of host and port are where we look for it in our browser
     # e.g. http://0.0.0.0:8080/
     # setting debug=True to see error messages - wouldn't do this on a final deployed app
